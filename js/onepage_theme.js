1e1b79dbbad8cd0a2289a8bd7264fa0171b4881e
Chris Keller <chris@commercialprogression.com>
2014-03-18 21:41:15 -0400

    Added ddscrollspy and adjusted some styles.

--- js/onepage_theme.js
+++ js/onepage_theme.js
@@ -2,14 +2,8 @@
 
   Drupal.behaviors.onepage_theme = {
     attach: function(context, settings) {
-      // Smooth scroll to anchor links on front page
-      var $html_body = $('html, body');
-      var anchor_links = $('.view-display-id-anchor_block .views-row a');
-      anchor_links.click(function() {
-        $html_body.animate({
-          scrollTop: $($.attr(this, 'href')).offset().top - 100
-        }, 500);
-        return false;
+      $('.view-display-id-anchor_block .link-list').ddscrollSpy({
+        scrolltopoffset: -100
       });
     }
   };
